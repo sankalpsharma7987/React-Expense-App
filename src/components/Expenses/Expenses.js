9f65567414f392e7bf0cd590d93ea1905bcfa3f7
sankalpsharma7987@gmail.com <sankalpsharma7987@gmail.com>
2021-05-27 10:30:36 -0400

    Code Refactoring for filtered Expenses logic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,49 +1,53 @@
-import {useState} from 'react';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpenseFilter from './ExpenseFilter';
-import './Expenses.css';
+import { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import "./Expenses.css";
 
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
 
-const Expenses = (props)=> {
+  const expenseFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const [filteredYear,setFilteredYear] = useState('2021');
-    
-    const expenseFilterChangeHandler = selectedYear =>{
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
 
-        setFilteredYear(selectedYear);
+  let expensesContent = <p> No expenses found </p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          expenseId={expense.id}
+          expenseDate={expense.date}
+          expenseDescription={expense.title}
+          expenseAmount={expense.amount}
+        />
+      );
+    });
+  }
+
+  return (
+
+    <div>
+
+      <Card className="expenses">
+
+        <ExpenseFilter
+          defaultFilterValue={filteredYear}
+          onFilterChange={expenseFilterChangeHandler}
+        />
+
+        {expensesContent}
         
-        
-    }
-
-    const filteredExpenses = props.expenses.filter(expense=>expense.date.getFullYear().toString()===filteredYear)
-
-    return (
-        
-        <div>
-            
-            <Card className = "expenses">
-            <ExpenseFilter defaultFilterValue = {filteredYear} onFilterChange = {expenseFilterChangeHandler}></ExpenseFilter>
-            
-            {
-            filteredExpenses.map(expense=>{
-
-                return <ExpenseItem 
-                key = {expense.id} 
-                expenseId = {expense.id} 
-                expenseDate = {expense.date} 
-                expenseDescription = {expense.title} 
-                expenseAmount = {expense.amount}
-                />
-
-            })
-            }
-
-            </Card>
-
-        </div>
-    );
-    
-}
+      </Card>
+      
+    </div>
+  );
+};
 
 export default Expenses;
